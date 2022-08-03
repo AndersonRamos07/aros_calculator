6929ffb8f07483379e6555996ac499158db221e0
AndersonRamos07 <>
2022-08-03 04:29:30 +0000

    testando o primeiro commit

--- script.js
+++ script.js
@@ -24,28 +24,47 @@ const mostraNoVisor = ()=>{
 }
 
 const porNoVisor = (p)=>{
+    if(p == "zerar"){
+        return domQS("#valor", 1, 0)
+    }else{
     var valueBefore = domQS("#valor", 0, 0);
-    var valueFinal = valueBefore.concat(p)
-    return domQS("#valor", 1, valueFinal)//document.querySelector("#valor").innerHTML = valueFinal;
+    var valueFinal = valueBefore.concat(p);
+    return domQS("#valor", 1, valueFinal);
+    }
 }
 
 const cSomente = ()=>{
- return domQS("#valor", 1, 0)
-  //document.querySelector("#valor").innerHTML = 0;
+ return domQS("#valor", 1, 0);
 }
 
 const cEE = ()=>{
-  var dut = domQS("#valor", 0, 0)
-  //document.querySelector("#valor").innerHTML;
-  //console.log(dut.length + " <dut.length>")
-  var resultar = dut.slice(0, -1)
-  return domQS("#valor", 1, resultar)
-  //document.querySelector("#valor").innerHTML = resultar;
-  //console.log(resultar + " <resultar>")
+  var dut = domQS("#valor", 0, 0);
+  var resultar = dut.slice(0, -1);
+  return domQS("#valor", 1, resultar);
 }
 
 const operacao = (p)=>{
-    var initialTerm = domQS("#valor", 1, 0);
+    var initialTerm = domQS("#valor", 0, 0);
+    console.log("inicialTerm: " + initialTerm)
+    var result;
+    switch(p){
+        case "soma":
+        result = initialTerm + " +";
+        break;
+        case "subt":
+        result = initialTerm + " -";
+        break;
+        case "mult":
+        result = initialTerm + " x";
+        break;
+        case "divi":
+        result = initialTerm + " /";
+        break;
+    }
+    porNoVisor("zerar"); 
+    var comOperando = domQS("#calculo", 1, result)
+    console.log(comOperando + " <comOperando>")
+    //console.log(result + " <result>")
     
 }
 
