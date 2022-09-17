c021cc991b1f6a8ada200ee7399feb068267034f
AndersonRamos07 <andersonramos07@live.com>
2022-09-17 01:25:57 -0400

    organizando...

--- public/btnNumbers.js
+++ public/btnNumbers.js
@@ -1,12 +1,20 @@
 const getNumber = (pNumber) => {
-    let isItOn = element(pSwtc).checked;
-    isItOn?
-    show(pNumber) :
-    console.log(isItOn);
+    let vIsItOn = element(pSwtc).checked;
+    vIsItOn?
+    toShow(pNumber) :
+    console.log(vIsItOn);
 };
 
-const show = (pValue) => {
+const toShow = (pValue) => {
+    let vElement = element(pMain).innerText;
+    vElement == "."? 
+       toMerge(0, pValue):
+       toMerge(vElement, pValue);
+};
+
+const toMerge = (pValuePrev, pValueNext) =>{
     let vElement = element(pMain);
-    console.log(pValue);
-    toSetLabel(vElement, pValue);
-}
+    pValuePrev >= 0?
+        toSetLabel(vElement, pValuePrev + pValueNext):
+        toSetLabel(vElement, pValueNext);
+};
