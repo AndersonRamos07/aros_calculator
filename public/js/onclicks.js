5f1b919c83d1584116d9a155c6cfd2fb56089930
AndersonRamos07 <andersonramos07@live.com>
2022-12-02 14:56:58 -0500

    add button functions - DEL MEMO

--- public/js/onclicks.js
+++ public/js/onclicks.js
@@ -5,7 +5,7 @@ let visor_Sub = elementDOM('#visorSub', 1);
 
 let btn_C = elementDOM('#btnC', 1);
 let btn_CE = elementDOM('#btnCE', 1);
-let btn_DEL = elementDOM('#btncDEL', 1);
+let btn_DEL = elementDOM('#btnDEL', 1);
 let btn_MEMO = elementDOM('#btnMEMO', 1);
 
 let btn_DOT = elementDOM('#btnDOT', 1);
@@ -16,11 +16,24 @@ let dataList = elementDOM('datalist', 1);
 let btns_NUMB = elementDOM('.numbers > button', 0);
 let btns_OPERATORS = elementDOM('.operators > button', 0);
 
+let tb = elementDOM('table', 1);
+
+// ******* SUB_ELEMENTS
+
+let operator = [ " + " , " - " , " x " , " ÷ " ]
+/*
+let vM_txtC = visor_Main.textContent;
+let vM_iTxt = visor_Main.innerText;
+
+let vS_txtC = visor_Sub.textContent;
+let vS_iTxt = visor_Sub.innerText;
+*/
 
 // ******* EVENTS
 
 eventListener(btn_C, 'click', toZero);
 eventListener(btn_CE, 'click', toClear);
+eventListener(btn_DEL, 'click', toDel);
 
 btns_NUMB.forEach(
     btn => btn.addEventListener('click', getDigit,
@@ -31,3 +44,5 @@ btns_OPERATORS.forEach(
     btn => btn.addEventListener('click', getOperator,
             {capture: false})
 );
+
+//eventListener(btn_MEMO, 'click', toCreateEquation)
