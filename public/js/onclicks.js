d5b4e88128862e319215bda6a9cb3a869f96e862
Anderson Ramos <andersonramos07@live.com>
2023-01-01 04:43:29 -0500

    corrections in onclicks.js

--- public/js/onclicks.js
+++ public/js/onclicks.js
@@ -1,44 +1,27 @@
 //#region ******* ELEMENTS
-
 let visor_Main = elementDOM('#visorMain', 1);
 let visor_Sub = elementDOM('#visorSub', 1);
+let tb = elementDOM('table', 1);
 
 let btn_C = elementDOM('#btnC', 1);
 let btn_CE = elementDOM('#btnCE', 1);
 let btn_DEL = elementDOM('#btnDEL', 1);
 let btn_MEMO = elementDOM('#btnMEMO', 1);
 
-let btn_DOT = elementDOM('#btnDOT', 1);
-let btn_EQUALS = elementDOM('#btnEQUALS', 1);
+let btn_DOT = elementDOM('#dot', 1);
+let btn_EQUALS = elementDOM('#eql', 1);
 
-let dataList = elementDOM('datalist', 1);
-
-let btns_NUMB = elementDOM('.numbers > button', 0);
-let btns_OPERATORS = elementDOM('.operators > button', 0);
-
-let tb = elementDOM('table', 1);
-//#endregion
-
-//#region ******* SUB_ELEMENTS
-
-let operator = [ " + " , " - " , " x " , " ÷ " ];
+let btns_MAIN = elementDOM('main > section > button', 0);
 //#endregion
 
 //#region ******* EVENTS
+btn_C.addEventListener('click', (e) => toClear());
+btn_CE.addEventListener('click', (e) => toClearIn());
+btn_DEL.addEventListener('click', (e) => toDel());
+btn_MEMO.addEventListener('click', (e) => toPutTable());
+btn_EQUALS.addEventListener('click', (e) => toCalculate());
 
-eventListener(btn_C, 'click', toClear);
-eventListener(btn_CE, 'click', toClearIn);
-eventListener(btn_DEL, 'click', toDel);
-eventListener(btn_EQUALS, 'click', toCalculate);
-eventListener(btn_MEMO, 'click', toPutTable);
-
-btns_NUMB.forEach(
+btns_MAIN.forEach(
     btn => btn.addEventListener('click', getDigit,
         {capture: false})
 );
-
-btns_OPERATORS.forEach(
-    btn => btn.addEventListener('click', getOperator,
-            {capture: false})
-);
-//#endregion
