6f4a1ddb851f3a09fe425747bdfa84cf5c222859
Anderson Ramos <andersonramos07@live.com>
2023-01-01 13:15:37 -0500

    refactoring the events

--- public/js/onclicks.js
+++ public/js/onclicks.js
@@ -8,9 +8,6 @@ let btn_CE = elementDOM('#btnCE', 1);
 let btn_DEL = elementDOM('#btnDEL', 1);
 let btn_MEMO = elementDOM('#btnMEMO', 1);
 
-let btn_DOT = elementDOM('#dot', 1);
-let btn_EQUALS = elementDOM('#eql', 1);
-
 let btns_MAIN = elementDOM('main > section > button', 0);
 //#endregion
 
@@ -19,7 +16,6 @@ btn_C.addEventListener('click', (e) => toClear());
 btn_CE.addEventListener('click', (e) => toClearIn());
 btn_DEL.addEventListener('click', (e) => toDel());
 btn_MEMO.addEventListener('click', (e) => toPutTable());
-btn_EQUALS.addEventListener('click', (e) => toCalculate());
 
 btns_MAIN.forEach(
     btn => btn.addEventListener('click', getDigit,
